f5dbbc85b02a5e4c991bb15009b764940fdf8ebc
Maxime <maxime.le-bot@universite-paris-saclay.fr>
2021-11-23 00:27:18 +0100

    Nouvelle Cam + mur/map

--- travail/projet test/Classes/Resource/Camera.cpp
+++ travail/projet test/Classes/Resource/Camera.cpp
@@ -1,5 +1,4 @@
 
-/* Generated from GenMyModel */
 
 #include "..\Header\Camera.hpp"
 #include "..\Header\MoveableObject.hpp"
@@ -8,33 +7,54 @@
 
 
 Camera::Camera() {
+    _x = 0;
+    _y = 0;
+    _window.setSize(sf::Vector2u(1920, 1080));
+    _window.setTitle("Super Dédale Bros ULtimate 2 feat. Dante from Devil May Cry EXTENDED Edition ver 1.246859553");
+    _window.setFramerateLimit(60);
+}
 
+Camera::Camera(int x, int y) {
+    _x = x;
+    _y = y;
+    _window.setSize(sf::Vector2u(1920, 1080));
+    _window.setTitle("Super Dédale Bros ULtimate 2 feat. Dante from Devil May Cry EXTENDED Edition ver 1.246859553");
+    _window.setFramerateLimit(60);
+}
+
+sf::RenderWindow& Camera::getWindow() {
+    return _window;
 }
 
 
-
-void Camera::drawMap(const Map m, sf::RenderWindow& window)const{
-
-}
-
-void Camera::drawAll(const vector<DrawableObject>& objectList, sf::RenderWindow& window, const Map m)const{
-    while (window.isOpen()) {
-        sf::CircleShape shape(100.f);
-        shape.setFillColor(sf::Color::Green);
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-        window.clear();
-        drawMap(m, window);
-        for (uint32_t i = 0; i < objectList.size(); i++) {
-            drawObject(objectList.at(i), window);
-        }
-        window.display();
+void Camera::drawMap(Map m) {
+    //dessin des murs :
+    for (int i = 0; i < m.getWallList().size(); i++) {
+        sf::Vertex line[] = {
+            //créer uun vecteur avec les 2 coordonnées stockées dans WallList
+            sf::Vertex(sf::Vector2f(m.getWallList().at(i).getPoint1().x, m.getWallList().at(i).getPoint1().y)),
+            sf::Vertex(sf::Vector2f(m.getWallList().at(i).getPoint2().x, m.getWallList().at(i).getPoint2().y))
+        };
+        _window.draw(line, 2, sf::Lines);
     }
 }
 
-void Camera::drawObject(const DrawableObject& object, sf::RenderWindow& window)const {
-    
+void Camera::drawAll(const list<MoveableObject*> objectList, const Map m) {
+    while (_window.isOpen()) {
+        sf::Event event;
+        while (_window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                _window.close();
+        }
+        _window.clear();
+        drawMap(m);
+        for (MoveableObject* object : objectList) {
+            drawObject(object);
+        }
+        _window.display();
+    }
+}
+
+void Camera::drawObject(const MoveableObject* object)const {
+
 }
--- travail/projet test/Classes/Resource/Map.cpp
+++ travail/projet test/Classes/Resource/Map.cpp
@@ -1,4 +1,27 @@
 
-/* Generated from GenMyModel */
 
 #include "..\Header\Map.hpp"
+#include <vector>
+
+Map::Map() {
+	Wall wall1;
+	sf::Vector2f p1, p2;
+	p1.x = 0;
+	p1.y = 0;
+	p2.x = 200;
+	p2.y = 0;
+	Wall wall2(p1, p2);
+	p1.y = 200;
+	p2.y = 200;
+	Wall wall3(p1, p2);
+	p1.x = 200;
+	p1.y = 0;
+	Wall wall4(p1, p2);
+	wallList.push_back(wall1);
+	wallList.push_back(wall2);
+	wallList.push_back(wall3);
+	wallList.push_back(wall4);
+}
+std::vector<Wall> Map::getWallList() {
+	return wallList;
+}
