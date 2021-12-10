47ae9f3c84ef9113d5e9bd5bb8a715e9ebe083a7
Theophile Vaudour <theophile.vaudour@u-psud.fr>
2021-12-10 12:13:41 +0100

    added things in hittablecharacter

--- as-pts1/travail/projet test/Classes/Resource/HittableCharacter.cpp
+++ as-pts1/travail/projet test/Classes/Resource/HittableCharacter.cpp
@@ -6,6 +6,7 @@
 HittableCharacter::HittableCharacter()
 {
     _health = 10;
+    _equippedWeapon = nullptr;
 }
 
 void HittableCharacter::setEquippedWeapon(Weapon* newWeapon)
@@ -13,8 +14,25 @@ void HittableCharacter::setEquippedWeapon(Weapon* newWeapon)
     _equippedWeapon = newWeapon;
 }
 
+void HittableCharacter::setHealth(double health)
+{
+    _health = health;
+}
+
+void HittableCharacter::changeHealth(double modificateur)
+{
+    if (modificateur >= 0)
+    {
+        _health += modificateur;
+    }
+    else
+    {
+        _health -= modificateur;
+    }
+}
 
 Weapon* HittableCharacter::getEquippedWeapon()
 {
     return _equippedWeapon;
 }
+
